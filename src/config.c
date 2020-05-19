dbe22b47064fd5a033ab7643ed8fb1b0a4d9343e
jjos2372 <jjos2372@gmail.com>
2020-05-18 22:18:00 -0300

    Another special identifier

--- src/config.c
+++ src/config.c
@@ -9,7 +9,7 @@
 
 const uint8_t SUPPORTED_TXN_VERSION = 1;
 
-const uint8_t BURST_SPECIAL_IDENTIFIER[] = {0xba, 0xbb, 0xbc};
+const uint8_t BURST_SPECIAL_IDENTIFIER[] = {0x0a, 0x0b, 0x0c};
 const uint8_t BURST_SPECIAL_IDENTIFIER_LEN = sizeof(BURST_SPECIAL_IDENTIFIER);
 
 const uint8_t VERSION_FLAGS = 0x00;
